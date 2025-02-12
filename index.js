d44c29bba3d7fa8cced5c15ade201f24e6b82219
Choi Yeonung <yonsweng@gmail.com>
2025-02-12 21:48:08 +0900

    Make a first bot

---
+++ index.js
@@ -0,0 +1,55 @@
+// 주요 클래스 가져오기
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const cron = require('node-cron');
+const { STUDY_CHANNEL_ID, token } = require('./config.json');
+
+// 클라이언트 객체 생성 (Guilds관련, 메시지관련 인텐트 추가)
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, 
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+]});
+
+MONDAY_MESSAGE = '@everyone 토요일 목표는 다 했어? 오늘은 뭐 할거야?';
+SCHEDULE_MESSAGE = '@everyone 어제 목표는 다 했어? 오늘은 뭐 할거야?';
+
+// 봇이 준비됐을때 한번만(once) 표시할 메시지
+client.once(Events.ClientReady, readyClient => {
+    console.log(`${readyClient.user.tag}이 로그인했다.`);
+
+    cron.schedule('30 17 * * 1', async () => {
+        const channel = await client.channels.fetch(STUDY_CHANNEL_ID);
+        if (channel) {
+            await channel.send(MONDAY_MESSAGE);
+        }
+    }, {
+        timezone: 'Asia/Seoul'
+    });
+
+    cron.schedule('30 17 * * 2-5', async () => {
+        const channel = await client.channels.fetch(STUDY_CHANNEL_ID);
+        if (channel) {
+            await channel.send(SCHEDULE_MESSAGE);
+        }
+    }, {
+        timezone: 'Asia/Seoul'
+    });
+
+    cron.schedule('0 12 * * 6', async () => {
+        const channel = await client.channels.fetch(STUDY_CHANNEL_ID);
+        if (channel) {
+            await channel.send(SCHEDULE_MESSAGE);
+        }
+    }, {
+        timezone: 'Asia/Seoul'
+    });
+});
+
+client.on('messageCreate', async message => {
+    if (message.content === '다빈아 안녕?') {
+        await message.channel.send('안녕!');
+    }
+});
+
+// 시크릿키(토큰)을 통해 봇 로그인 실행
+client.login(token);

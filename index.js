d61d77678c6bc6e8aa0a62f70156562c6b10542b
Choi Yeonung <39218496+yonsweng@users.noreply.github.com>
2025-03-04 22:17:22 +0900

    fix: auth problem (#10)

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ import { Client, Events, GatewayIntentBits } from 'discord.js';
 import cron from 'node-cron';
 import express from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
-import axios from 'axios';
+import { GoogleAuth } from 'google-auth-library';
 
 dotenv.config();
 
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
 const DISCORD_LOGIN_TOKEN = process.env.DISCORD_LOGIN_TOKEN;
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const ENGINE_URL = process.env.ENGINE_URL;
-const ENGINE_TOKEN = process.env.ENGINE_TOKEN;
 const app = express();
 const PORT = 8080;
 
@@ -24,10 +21,6 @@ const client = new Client({ intents: [
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
 ]});
-
-const openai = new OpenAI({
-    apiKey: OPENAI_API_KEY,
-});
 
 const MONDAY_MESSAGE = '@everyone 토요일 목표는 다 했어? 오늘은 뭐 할거야?';
 const SCHEDULE_MESSAGE = '@everyone 어제 목표는 다 했어? 오늘은 뭐 할거야?';
@@ -69,6 +62,8 @@ client.once(Events.ClientReady, async readyClient => {
     });
 });
 
+const auth = new GoogleAuth();
+
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
 
@@ -76,14 +71,15 @@ client.on('messageCreate', async message => {
         const prompt = message.content.replace(`<@${client.user.id}>`, '').trim();
         if (prompt) {
             try {
+                const client = await auth.getIdTokenClient(ENGINE_URL);
                 const requestBody = {
                     messages: [prompt],
                     thread_id: DISCORD_CHANNEL_ID,
                 };
-                const response = await axios.post(`${ENGINE_URL}/messages`, requestBody, {
-                    headers: {
-                        Authorization: `Bearer ${ENGINE_TOKEN}`
-                    }
+                const response = await client.request({
+                    url: `${ENGINE_URL}/messages`,
+                    method: 'POST',
+                    data: requestBody
                 });
                 const reply = response.data.response.content;
                 await message.channel.send(reply);
